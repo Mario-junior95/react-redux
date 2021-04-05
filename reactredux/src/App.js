48919a492ad83a60acd3393f28d16324e88458c4
Mario-junior95 <karehmario@gmail.com>
2021-04-05 22:06:31 +0300

    ownProps

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -8,11 +8,14 @@ import IceCreamContainer from "./Components/IceCreamContainer";
 import CocktailContainer from "./Components/CocktailContainer";
 import HooksCocktailContainer from "./Components/HooksCocktailContainer";
 import NewCakeContainer from "./Components/NewCakeContainer";
+import ItemContainer from "./Components/ItemContainer";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <CakeContainer />
         <HooksCakeContainer />
         <IceCreamContainer />
